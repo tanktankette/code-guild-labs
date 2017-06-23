33177d0eacee4a5ea9c26546803934ad0871866c
Nick Riehl <rhontos@gmail.com>
2017-06-23 14:37:02 -0700

    Finished vowel lab

---
+++ js/vowel/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tanktankette on 6/23/17.
+ */
+alert("It's time to play...")
+alert("IS IT A VOWEL!!!!!!!")
+
+var c = prompt('Enter a character');
+
+while(c !== ""){
+    c.toLowerCase();
+    if(c === "a" || c === "e" || c === "i" || c === "o" || c === "u"){
+        alert("IT'S A VOWEL! YOU WIN THE GAME!")
+    } else if (c === "y") {
+        alert("It's a... a 'Y'. Don't know what to do what that... Congrats anyway?")
+    } else if (c.length > 1){
+        alert("It's not a valid input!")
+    } else {
+        alert("It's not a vowel!")
+    }
+    var c = prompt('Enter a character');
+}
+

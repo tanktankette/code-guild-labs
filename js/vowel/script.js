38261de2d2f10d07db9fa5767ef9cfe68e59c9c0
Nick Riehl <rhontos@gmail.com>
2017-06-23 14:38:17 -0700

    fixed type -.-

--- js/vowel/script.js
+++ js/vowel/script.js
@@ -2,7 +2,7 @@
  * Created by tanktankette on 6/23/17.
  */
 alert("It's time to play...")
-alert("IS IT A VOWEL!!!!!!!")
+alert("IS IT A VOWEL?!?!?!")
 
 var c = prompt('Enter a character');
 
@@ -11,7 +11,7 @@ while(c !== ""){
     if(c === "a" || c === "e" || c === "i" || c === "o" || c === "u"){
         alert("IT'S A VOWEL! YOU WIN THE GAME!")
     } else if (c === "y") {
-        alert("It's a... a 'Y'. Don't know what to do what that... Congrats anyway?")
+        alert("It's a... a 'Y'. Don't know what to do with that... Congrats anyway?")
     } else if (c.length > 1){
         alert("It's not a valid input!")
     } else {
